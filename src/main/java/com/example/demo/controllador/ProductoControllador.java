faaff87780ac6f0317bd944000b952cbbdf23578
luis07 <luis07@gmail.com>
2023-11-26 01:54:33 -0500

    Commit 2

--- src/main/java/com/example/demo/controllador/ProductoControllador.java
+++ src/main/java/com/example/demo/controllador/ProductoControllador.java
@@ -1,9 +1,14 @@
 package com.example.demo.controllador;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entidad.Producto;
 import com.example.demo.repositorio.interfacepro;
 @Controller
 
@@ -18,8 +23,29 @@ public class ProductoControllador {
 		
 		return "productolist";
 		
+
+	}
+
+	@GetMapping ("/registrar")
+	public String registrarProducto(Model model) {
+		model.addAttribute("registpro",new Producto());
+		return "registrarProducto";
 		
+	}
+	
+	@PostMapping("/SafeProducto")
+	public String GuardarProducto(Model model,@ModelAttribute Producto registpro) {
+		interproduc.save(registpro);
 		
+		return "redirect:/productos";
+		
+	}
+	
+	@GetMapping ("/Actualizar/{idproducto}")
+	public String ActualizarProducto(Model model, @PathVariable Integer idproducto) {
+		model.addAttribute("registpro",interproduc.findById(idproducto));
+	
+		return "registrarProducto";
 	}
 	
 	
